1cef154ad5f8de59150992b3eaa3fe00487f9610
Пользователь <budkin.mihail@yandex.ru>
2021-08-10 16:26:40 +0300

    Nothing speacial

--- tgbot1.py
+++ tgbot1.py
@@ -1,30 +1,40 @@
 import logging
 import settings
-from telegram.ext import Updater, CommandHandler, callbackcontext, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
 logging.basicConfig( format='%(asctime)s - %(levelname)s - %(message)s',
 					level = logging.INFO,
 					filename='bot.log'
 					)
 
-def start_bot(update: Updater, context: callbackcontext):
+def start_bot(update:Updater, context: CallbackContext):
 	print(update)
-	mytext = """ Приветствую, {}
-	Скоро меня обучат и я буду очень умным. Be patient with me""".format(update.message.chat.first_name)
-	logging.info('User {} started dialog'.format(update.message.chat.first_name))
-	update.message.reply_text(mytext)
+	first = """Добро пожаловать, {}
+Узнать существующие команды напишите /help""".format(update.message.chat.first_name)
+	update.message.reply_text(first)
 
-def chat(update: Updater, context: callbackcontext):
-	answer = "Я не настолько умён, чтобы понимать тебя"
+def chat(update:Updater, context:CallbackContext):
+	print(update)
+	second = "Думаю лучше всего написать /help"
+	update.message.reply_text(second)
 
-	update.message.reply_text(answer)
-
+def helper(update:Updater, context:CallbackContext):
+	print(update)
+	third = """В данный момент есть 2 команды /help и /start
+В будущем должны появиться ещё несколько"""
+	update.message.reply_text(third)
+	
 def main():
-	updtr = Updater (settings.TOKEN_TELEGRAM)
-	updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+	updtr = Updater(settings.TOKEN_TELEGRAM)
+
+	updtr.dispatcher.add_handler(CommandHandler('start', start_bot))
+	updtr.dispatcher.add_handler(CommandHandler('help', helper))	
 	updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+	
+
 	updtr.start_polling()
-	updtr.idle
+	updtr.idle()
+
 if __name__ == "__main__":
 	logging.info("Bot Started")
 	main()
