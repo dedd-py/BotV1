5bcade2a91c7814557b85ca5aa7d28a531451a30
Пользователь <budkin.mihail@yandex.ru>
2021-08-13 14:33:20 +0300

    Добавилась функция будильника

--- tgbot1.py
+++ tgbot1.py
@@ -1,11 +1,13 @@
 import logging
 import settings
+from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
 logging.basicConfig( format='%(asctime)s - %(levelname)s - %(message)s',
 					level = logging.INFO,
 					filename='bot.log'
 					)
+logger = logging.getLogger(__name__)
 
 def start_bot(update:Updater, context: CallbackContext):
 	print(update)
@@ -13,24 +15,60 @@ def start_bot(update:Updater, context: CallbackContext):
 Узнать существующие команды напишите /help""".format(update.message.chat.first_name)
 	update.message.reply_text(first)
 
-def chat(update:Updater, context:CallbackContext):
-	print(update)
-	second = "Думаю лучше всего написать /help"
-	update.message.reply_text(second)
+def alarm(context: CallbackContext):
+	job = context.job
+	context.bot.send_message(job.context, text='Время вышло!')
+
+
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+def set_timer(update: Update, context: CallbackContext):
+	chat_id = update.message.chat_id
+	try:
+		# args[0] should contain the time for the timer in seconds
+		due = int(context.args[0])
+		if due < 0:
+			update.message.reply_text('Назад в будущее! Жаль, но не получилось')
+			return
+
+		job_removed = remove_job_if_exists(str(chat_id), context)
+		context.job_queue.run_once(alarm, due, context=chat_id, name=str(chat_id))
+
+		text = 'Таймер установлен!'
+		if job_removed:
+			text += ' Старый таймер был удалён.'
+		update.message.reply_text(text)
+
+	except (IndexError, ValueError):
+		update.message.reply_text('Используйте команду вот так: /set <секунды>')
+
+def unset(update: Update, context: CallbackContext):
+	chat_id = update.message.chat_id
+	job_removed = remove_job_if_exists(str(chat_id), context)
+	text = 'Таймер удалён!' if job_removed else 'У вас нет активных таймеров.'
+	update.message.reply_text(text)
 
 def helper(update:Updater, context:CallbackContext):
 	print(update)
-	third = """В данный момент есть 2 команды /help и /start
-В будущем должны появиться ещё несколько"""
+	third = """Появилась команда /set, чтобы поставить таймер
+	А если будильник вам больше не нужен будильник, то напишите /unset"""
 	update.message.reply_text(third)
-	
+
 def main():
 	updtr = Updater(settings.TOKEN_TELEGRAM)
 
 	updtr.dispatcher.add_handler(CommandHandler('start', start_bot))
-	updtr.dispatcher.add_handler(CommandHandler('help', helper))	
-	updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
-	
+	updtr.dispatcher.add_handler(CommandHandler('help', helper))
+	updtr.dispatcher.add_handler(CommandHandler("set", set_timer))
+	updtr.dispatcher.add_handler(CommandHandler("unset", unset))
 
 	updtr.start_polling()
 	updtr.idle()
